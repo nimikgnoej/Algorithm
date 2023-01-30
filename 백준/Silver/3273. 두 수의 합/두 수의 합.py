10d6d71308167662285064f8e25abf9f08f34558
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2023-01-30 15:08:06 +0900

    Create 두 수의 합.py

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+sys.stdin = open('input.txt', 'r')
+
+n = int(sys.stdin.readline().rstrip())
+arr = list(map(int,sys.stdin.readline().split()))
+x = int(sys.stdin.readline().rstrip())
+
+exists = [False] * 2000000
+
+
+cnt=0
+for i in range(n):
+  if (x-arr[i]) > 0 and exists[x-arr[i]]:
+    cnt+=1
+  exists[arr[i]] = True
+
+print(cnt)
