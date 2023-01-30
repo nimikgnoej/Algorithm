8d3f9f23be5b64d0dea27f75569091f0ba9c41a3
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2023-01-30 15:04:14 +0900

    [Silver III] Title: 두 수의 합, Time: 180 ms, Memory: 126244 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -5,14 +5,19 @@ from collections import deque
 n = int(sys.stdin.readline().rstrip())
 arr = list(map(int,sys.stdin.readline().split()))
 x = int(sys.stdin.readline().rstrip())
-
-exists = [False] * 2000000
-
+arr.sort()
 
 cnt=0
-for i in range(n):
-  if (x-arr[i]) > 0 and exists[x-arr[i]]:
+left = 0
+right = n-1
+while left < right:
+  if arr[left]+arr[right] == x:
     cnt+=1
-  exists[arr[i]] = True
+    left+=1
+    right-=1
+  elif arr[left]+arr[right]>x:
+    right-=1
+  else:
+    left+=1
 
 print(cnt)
