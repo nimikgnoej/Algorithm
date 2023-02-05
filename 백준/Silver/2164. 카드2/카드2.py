8d66ab0e0e51544a171354b25db2cf554257b022
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2023-02-05 18:02:51 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 128136 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -2,14 +2,14 @@ import sys
 from collections import deque
 #sys.stdin = open('input.txt', 'r')
 
-q = deque()
-n = int(sys.stdin.readline().rstrip())
+q=deque()
+n=int(sys.stdin.readline().rstrip())
 for i in range(n):
   q.append(i+1)
 
 while True:
-  if len(q) == 1:
-    print(q.pop())
+  if len(q)==1:
+    print(q[0])
     break
   q.popleft()
   q.append(q.popleft())
