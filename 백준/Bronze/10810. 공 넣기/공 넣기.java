d1d6e7fa45f5b9e9f45bf5f928b04d1daac46d82
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-05-13 22:34:44 +0900

    [Bronze III] Title: 공 넣기, Time: 152 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public void sol() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        int[] arr = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int from = Integer.parseInt(inputs[0])-1;
+            int to = Integer.parseInt(inputs[1])-1;
+            int input = Integer.parseInt(inputs[2]);
+            for(int j = from; j <= to; j++)
+                arr[j] = input;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        br.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().sol();
+    }
+}
