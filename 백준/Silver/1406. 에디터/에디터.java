74cc3112fd5b92af8d99aa3a80d9a7e01296d046
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-05-15 15:23:56 +0900

    [Silver II] Title: 에디터, Time: 416 ms, Memory: 55180 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -0,0 +1,65 @@
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int M = Integer.parseInt(br.readLine());
+
+        LinkedList<Character> list = new LinkedList<Character>();
+
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+        }
+
+        //iterator 메소드 호출
+        ListIterator<Character> iter = list.listIterator();
+        //처음 커서는 문장의 맨 뒤에 있어야하기 때문에 커서를 맨뒤로 이동시켜줌 (ex. abc|)
+        while (iter.hasNext()) {
+            iter.next();
+        }
+
+        for (int i = 0; i < M; i++) {
+            String command = br.readLine();
+            char c = command.charAt(0);
+            switch (c) {
+                case 'L':
+                    if (iter.hasPrevious())
+                        iter.previous();
+
+                    break;
+                case 'D':
+                    if (iter.hasNext())
+                        iter.next();
+
+                    break;
+                case 'B':
+                    //remove()는 next()나 previous()에 의해 반환된 가장 마지막 요소를 리스트에서 제거함
+                    if (iter.hasPrevious()) {
+                        iter.previous();
+                        iter.remove();
+                    }
+                    break;
+                case 'P':
+                    char t = command.charAt(2);
+                    iter.add(t);
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        for (Character chr : list) {
+            bw.write(chr);
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
