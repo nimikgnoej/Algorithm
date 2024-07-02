dd15dfce4390bdb1634d59639a4e126b68ee1759
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-07-02 18:55:38 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  int a,b;
+  for(int i=0;i<t;i++)
+  { 
+    cin>>a>>b;
+    cout<<"Case #"<<i+1<<": "<<a+b<<'\n';
+  }
+}
