0ed62a3bbc33de2bc7ad21dffcf4d5dbaa8cf4ee
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2025-04-05 17:49:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,36 @@
-import sys
-from collections import deque
-#sys.stdin = open('input.txt', 'r')
-
-n = int(input())
-map = [[0 for _ in range(n+1)] for _ in range(n+1)]
-visited = [[0 for _ in range(n+1)] for _ in range(n+1)]
+N = int(input())
+village = []
+for i in range(N):
+    village.append(list(map(int, list(input()))))
+visited = [[0 for _ in range(N)] for _ in range(N)]
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-village = []
-
-for i in range(n):
-  map[i] = list(input())
-
+q = []
+result = []
 def bfs(x,y):
-  q=deque()
-  q.append((x,y))
-  visited[x][y] = 1
-  village.append(1)
-  while q:
-    x,y=q.popleft()
-    for i in range(4):
-      nx = x+dx[i]
-      ny = y+dy[i]
-      if 0<=nx<n and 0<=ny<n:
-        if map[nx][ny] == '1' and visited[nx][ny] == 0:
-          q.append((nx,ny))
-          visited[nx][ny]=1
-          village[-1]+=1
+    count = 1
+    visited[x][y] = 1
+    q.append((x,y))
+    while len(q) != 0:
+        data = q.pop(0)
+        x = data[0]
+        y = data[1]
+        for k in range(4):
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if nx >= 0 and nx < N and ny >= 0 and ny < N:
+                if village[nx][ny] == 1 and visited[nx][ny] == 0:
+                    q.append((nx,ny))
+                    visited[nx][ny] = 1
+                    count += 1
+    result.append(count)
 
-for i in range(n):
-  for j in range(n):
-    if map[i][j] == '1' and visited[i][j]==0:
-      bfs(i,j)
-village.sort()
-print(len(village))
-for i in range(len(village)):
-  print(village[i])
+
+for i in range(N):
+    for j in range(N):
+        if village[i][j] == 1 and visited[i][j] == 0:
+            bfs(i,j)
+print(len(result))
+answer = sorted(result)
+for i in range(len(result)):
+    print(answer[i])
