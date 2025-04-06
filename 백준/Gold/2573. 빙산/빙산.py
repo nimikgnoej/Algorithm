c7c46bea1ac9eff71976a5804a78721f780eb196
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2025-04-06 16:38:00 +0900

    [Gold IV] Title: 빙산, Time: 788 ms, Memory: 215172 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,60 +1,84 @@
-#import sys
 from collections import deque
-#sys.stdin = open('input.txt', 'r')
-
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
-n,m = map(int,input().split())
-ice = [list(map(int,input().split())) for _ in range(n)]
-visited = [[0 for _ in range(m)]for _ in range(n)]
-visit = [[0 for _ in range(m)]for _ in range(n)]
-arr=[]
+N,M = map(int,input().split())
+oceans = [[0 for _ in range(M)] for _ in range(N)]
+ices = [[0 for _ in range(M)] for _ in range(N)]
+q = deque()
+visited = [[0 for _ in range(M)] for _ in range(N)]
+iceCount = 0
+year = 0
+
+for i in range(N):
+    ices[i] = list(map(int,input().split()))
+
+def countWater():
+    global oceans
+    oceans = [[0 for _ in range(M)] for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if ices[i][j] != 0:
+                water = 0
+                for k in range(4):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    if nx>=0 and ny>=0 and nx<N and ny<M:
+                        if ices[nx][ny] == 0:
+                            water += 1
+                oceans[i][j] = water
 
 def melt():
-  visited = [[0 for _ in range(m)]for _ in range(n)]
-  for i in range(n):
-    for j in range(m):
-      cnt=0
-      if ice[i][j] != 0:
+    global ices
+    global oceans
+    for i in range(N):
+        for j in range(M):
+            if ices[i][j] != 0:
+                if ices[i][j] - oceans[i][j] < 0:
+                    ices[i][j] = 0
+                else:
+                    ices[i][j] = ices[i][j] - oceans[i][j]
+
+def BFS(x,y):
+    global iceCount
+    q.append((x,y))
+    visited[x][y] = 1
+    while q:
+        x,y = q.popleft()
         for k in range(4):
-          nx=i+dx[k]
-          ny=j+dy[k]
-          if ice[nx][ny] == 0 and visited[nx][ny] == 0:
-            cnt+=1
-        if ice[i][j]>=cnt:
-          ice[i][j]-=cnt
-        else:
-          ice[i][j]=0
-        visited[i][j] = 1
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if nx>=0 and ny>=0 and nx<N and ny<M:
+                if ices[nx][ny] != 0 and visited[nx][ny] == 0:
+                    q.append((nx,ny))
+                    visited[nx][ny] = 1
+    iceCount += 1
 
-def bfs(x,y):
-  q=deque()
-  q.append([x,y])
-  visit[x][y] =1
-  while q:
-    x,y=q.popleft()
-    for i in range(4):
-      nx = x+dx[i]
-      ny = y+dy[i]
-      if ice[nx][ny] != 0 and visit[nx][ny] == 0:
-        q.append([nx,ny])
-        visit[nx][ny] = 1
-  arr.append(1)
+def updateVisited():
+    global visited
+    visited = [[0] * M for _ in range(N)]
 
-cnt=0
+def allMelted():
+    for i in range(N):
+        for j in range(M):
+            if ices[i][j] != 0:
+                return False
+    return True
+
 while True:
-  for i in range(n):
-    for j in range(m):
-      if ice[i][j] != 0 and visit[i][j] == 0:
-        bfs(i,j)
-  if len(arr) > 1:
-    print(cnt)
-    break
-  elif len(arr) <=1 and sum(map(sum,ice)) == 0:
-    print("0")
-    break
-  melt()
-  visit = [[0 for _ in range(m)]for _ in range(n)]
-  arr= []
-  cnt+=1
+    updateVisited()
+    countWater()
+    melt()
+    for i in range(N):
+        for j in range(M):
+            if ices[i][j] != 0 and visited[i][j] == 0:
+                BFS(i,j)
+    if iceCount > 1:
+        print(year+1)
+        break
+    else:
+        if allMelted():
+            print(0)
+            break
+        year += 1
+        iceCount = 0
