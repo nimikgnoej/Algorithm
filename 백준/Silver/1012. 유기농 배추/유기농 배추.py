b014a6e3959239cd9ae66b42e6e874ac4315f9b7
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2025-05-13 22:00:36 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 114308 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,41 @@
-import sys
 from collections import deque
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+T = int(input())
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
 
-def bfs(x, y):
-  cnt = 1
-  q = deque()
-  q.append([x, y])
-  visited[x][y] = 1
-  while q:
-    x, y = q.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if 0 <= nx < n and 0 <= ny < m:
-        if field[nx][ny] == 1 and visited[nx][ny] == 0:
-          q.append([nx, ny])
-          visited[nx][ny] = 1
-          cnt += 1
-  baechu.append(cnt)
+def printBoard(board):
+    for i in range(M):
+        for j in range(N):
+            print(board[i][j], end=" ")
+        print()
 
+def bfs(q, x,y, visited,board):
+    q.append((x,y))
+    visited[x][y] = 1
+    while q:
+        x,y = q.popleft()
+        for k in range(4):
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if nx>=0 and nx<M and ny>=0 and ny<N:
+                if board[nx][ny] == 1 and visited[nx][ny] == 0:
+                    q.append((nx,ny))
+                    visited[nx][ny] = 1
 
-t = int(input())
-while t:
-  baechu = []
-  m, n, k = map(int, input().split())
-  field = [[0 for _ in range(m)] for _ in range(n)]
-  visited = [[0 for _ in range(m)] for _ in range(n)]
-  for _ in range(k):
-    x, y = map(int, input().split())
-    field[y][x] = 1
-  for i in range(n):
-    for j in range(m):
-      if field[i][j] == 1 and visited[i][j] == 0:
-        bfs(i, j)
-  print(len(baechu))
-  t -= 1
+for t in range(T):
+    M,N,K = map(int,input().split())
+    board = [[0 for _ in range(N)] for _ in range(M)]
+    visited = [[0 for _ in range(N)] for _ in range(M)]
+    q = deque()
+    count = 0
+    for i in range(K):
+        baechux,baechuy = map(int,input().split())
+        board[baechux][baechuy] = 1
+
+    for i in range(M):
+        for j in range(N):
+            if board[i][j] == 1 and visited[i][j] == 0:
+                bfs(q,i,j,visited,board)
+                count += 1
+    print(count)
