15e1f88b63fb58209553fea60bfd979020e5c68d
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2025-05-14 21:48:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 124384 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,26 +1,29 @@
-#import sys
 from collections import deque
-#sys.stdin = open('input.txt', 'r')
 
-n,k = map(int,input().split())
-time = [0]*100001
+N,K = map(int,input().split())
 
+board = [0 for _ in range(200010)]
+visited = [0 for _ in range(200010)]
+q = deque()
 def bfs(x):
-  q = deque()
-  q.append(x)
-  while q:
-    nx = q.popleft()
-    if nx == k:
-      return time[k]
-  
-    if nx+1<=100000 and time[nx+1] == 0:
-      q.append(nx+1)
-      time[nx+1] = time[nx]+1
-    if nx-1>=0 and time[nx-1] == 0:
-      q.append(nx-1)
-      time[nx-1] = time[nx]+1
-    if nx*2 <= 100000 and time[2*nx] == 0:
-      q.append(2*nx)
-      time[2*nx] = time[nx]+1
-res = bfs(n)  
-print(res)
+    q.append(x)
+    visited[x] = 1
+    while q:
+        x = q.popleft()
+        if x-1 >=0 and visited[x-1] == 0:
+            board[x-1] = board[x] + 1
+            q.append(x-1)
+            visited[x-1]=1
+        if x+1 <= 100001 and visited[x+1] == 0:
+            board[x + 1] = board[x] + 1
+            q.append(x+1)
+            visited[x + 1] = 1
+        if 2*x <= 100001 and visited[2*x] == 0:
+            board[x * 2] = board[x] + 1
+            q.append(2*x)
+            visited[2*x] = 1
+        if visited[K] == 1:
+            return
+
+bfs(N)
+print(board[K])
